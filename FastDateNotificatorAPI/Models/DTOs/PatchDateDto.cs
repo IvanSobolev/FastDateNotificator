750edd0d0527c1afb79b9bc1762eceaadff37ab6
IvanSobolev <i@bezlikiy.ru>
2025-05-09 12:08:02 +0300

    feat: dto for manager

---
+++ FastDateNotificatorAPI/Models/DTOs/PatchDateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FastDateNotificatorAPI.Models.DTOs;
+
+public class PatchDateDto (long id, string? name = null, DateOnly? dateOnly = null)
+{
+    public long Id { get; set; } = id;
+    public string? Name { get; set; } = name;
+    public DateOnly? DateOnly { get; set; } = dateOnly;
+}
