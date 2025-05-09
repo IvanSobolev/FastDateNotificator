5d76bc04175b743050d5378e4c1bfd95302e09a7
IvanSobolev <i@bezlikiy.ru>
2025-05-09 12:54:55 +0300

    feat: get all user id repository

--- FastDateNotificatorAPI/Repositories/Interfaces/IRememberedDateRepository.cs
+++ FastDateNotificatorAPI/Repositories/Interfaces/IRememberedDateRepository.cs
@@ -7,6 +7,7 @@ public interface IRememberedDateRepository
 {
     Task<Result> AddAsync(long telegramId, string name, DateOnly date);
     Task<Result<RememberedDate>> GetAsync(long id);
+    Task<Result<ICollection<long>>> GetAllUserIdAsync();
     Task<Result<ICollection<RememberedDate>>> GetForUserAsync(long telegramId);
     Task<Result<RememberedDate>> PatchAsync(long id, string? name = null, DateOnly? dateOnly = null);
     Task<Result<RememberedDate>> UpdateAsync(long id, long telegramId, string name, DateOnly date);
