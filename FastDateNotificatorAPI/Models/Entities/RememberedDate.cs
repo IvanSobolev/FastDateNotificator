8c97379a04f994de5dbac5e81e77a05cc6bf19f4
IvanSobolev <i@bezlikiy.ru>
2025-05-09 11:05:28 +0300

    fix: rename field

---
+++ FastDateNotificatorAPI/Models/Entities/RememberedDate.cs
@@ -0,0 +1,9 @@
+﻿namespace FastDateNotificatorAPI.Models.Entities;
+
+public class RememberedDate
+{
+    public long Id { get; set; }
+    public long TelegramId { get; set; }
+    public string Name { get; set; }
+    public DateOnly Date { get; set; }
+}
