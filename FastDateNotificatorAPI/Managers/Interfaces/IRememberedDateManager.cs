a203cf8c4888f9909da8ea316b5aa39782a4479e
IvanSobolev <i@bezlikiy.ru>
2025-05-09 12:08:34 +0300

    feat: interfaces

---
+++ FastDateNotificatorAPI/Managers/Interfaces/IRememberedDateManager.cs
@@ -0,0 +1,15 @@
+﻿using FastDateNotificatorAPI.Abstractions;
+using FastDateNotificatorAPI.Models.DTOs;
+using FastDateNotificatorAPI.Models.Entities;
+
+namespace FastDateNotificatorAPI.Managers.Interfaces;
+
+public interface IRememberedDateManager
+{
+    Task<Result> AddAsync(AddDateDto addDate);
+    Task<Result<RememberedDate>> GetAsync(long id);
+    Task<Result<ICollection<RememberedDate>>> GetForUserAsync(long telegramId);
+    Task<Result<RememberedDate>> PatchAsync(PatchDateDto patchDate);
+    Task<Result<RememberedDate>> UpdateAsync(UpdateDateDto updateDate);
+    Task<Result> DeleteAsync(long id);
+}
